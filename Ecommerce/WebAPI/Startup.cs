2a2fe679ab5e85e4adb79a15513eb22d35044c2f
sergiomirandaserjao <165831473+sergiomirandaserjao@users.noreply.github.com>
2024-05-30 21:02:58 -0300

    ProjetoFullStackECommerceDotNetCoreVuejs

---
+++ Ecommerce/WebAPI/Startup.cs
@@ -0,0 +1,181 @@
+using AutoFilterer.Swagger;
+using Core.ActionFilters;
+using Core.Configurations;
+using Core.DependencyResolvers;
+using Core.Utilities.Extensions;
+using Core.Utilities.Helpers;
+using Core.Utilities.IoC;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+            services.AddControllers().AddNewtonsoftJson(opts =>
+            {
+                opts.SerializerSettings.Converters.Add(new StringEnumConverter());
+                opts.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            })
+               .AddJsonOptions(opts => opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())); ;
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+                c.UseAutoFiltererParameters();
+            });
+
+
+            services.AddDbContext<ECommerceContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<ECommerceContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 9;
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = true;
+            });
+
+
+
+            services.Configure<JWTOptions>(Configuration.GetSection("JWTOptions"));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+            {
+                var jwtOptions = Configuration.GetSection("JWTOptions").Get<JWTOptions>();
+                opts.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidAudience = jwtOptions.Audience[0],
+                    IssuerSigningKey = SecurityKeyHelper.GetSymmetricSecurityKey(jwtOptions.SecurityKey),
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddDependencyResolvers(Configuration, new ICoreModule[] {
+               new CoreModule()
+            });
+
+            ValidatorOptions.Global.LanguageManager.Enabled = false;
+            services.AddScoped<NullFilterAttribute>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+            }
+            app.UseCustomExceptionMiddleware();
+            app.UseHttpsRedirection();
+
+	    var path = Path.Combine(env.ContentRootPath, "Uploads");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(env.ContentRootPath, "Uploads")),
+                RequestPath = "/Uploads"
+            });
+
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseCors(builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+            app.UseAuthorization();
+
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,67 @@
+# .NET5 with VueJS 3 Composition API (Quasar Framework) ECommerce Project Sérgio Miranda
+
+<img alt="Ecommerce Gif" src="assets/ecommerce.gif"> </img>
+
+**<h2 align="center">Features</p>**
+
+## Backend
+- .NET5
+- Entity Framework Core – Code First 
+- Response Wrappers
+- Repository Pattern
+- Action Filters
+- Automapper
+- Pagination,Search,Filter(Autofilterer)
+- Net Core Identity with JWT Authentication,Refresh Token
+- Role Based Authorization
+- Database Seeding
+- Custom Exception Handling Middleware
+- Complete User Management  (Register / Generate Token / Forgot Password / Confirmation Mail)
+- Logging (Serilog),Memory Caching,Validation (Fluent Validation),Transaction,Exception,Performance with Aspects (Autofac,Castle.DynamicProxy)
+
+## Frontend
+- Vue3
+- Composition API
+- Vuelidate
+- Vuex
+- Route guards
+- Dashboard
+
+## How To Start Asp Net Core API
+
+For asp net core, you must edit the appsettings.json file before typing these commands. 
+
+```sh
+dotnet ef migrations add CreateDatabase --context ECommerceContext --project "DataAccess" --startup-project "WebAPI"
+dotnet ef database update --context ECommerceContext --project "DataAccess" --startup-project "WebAPI"
+```
+After these commands, a database will be created. 
+Default Admin Account : 
+
+```sh
+Username : admin
+Password : 159357456qW
+```
+
+Checkout Page Credit Card Information : 
+
+```sh
+CardName : "John Doe"
+CardName : "5528790000000008"
+ExpirationMonth : "12"
+ExpirationYear : "2030"
+Cvc : "123"
+```
+
+## How To Start Quasar Project
+
+Project requires [Quasar Framework](https://quasar.dev) 
+
+
+```sh
+npm install
+quasar dev
+```
+
+
+
